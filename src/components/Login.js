93e92f41d62b3dd75887fc1c5f456a986691015f
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-03-19 17:32:25 -0700

    Add labels to Login form inputs.  Confirm auth works successfully.  Saves authToken to localStorage.  Redirects to /protected PrivateRoute and renders BubblePage component successfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,14 +56,15 @@ const Login = () => {
             id="username" 
             label="Username" 
             name="username" 
+            placeholder="Username"
             value={credentials.username} 
             onChange={handleChange}
           />
           <input 
             id="password" 
             label="Password" 
-            variant="filled" 
             name="password" 
+            placeholder="Password"
             value={credentials.password} 
             onChange={handleChange}
           />
@@ -83,8 +84,6 @@ const Login = () => {
 export default Login;
 
 //Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEXT "username" and "password" RESPECTIVELY.
+
 //4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
 //5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
