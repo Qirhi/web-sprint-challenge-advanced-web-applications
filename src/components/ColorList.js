04808a1473a81590b0c954ada8971c3609f2553d
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-03-19 18:38:03 -0700

    DeleteColor function in ColorList Component complete.  State is updated successfully.  Axios delete request returns successfully and components and sate update and render

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 
 import EditMenu from "./EditMenu";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
+
 
 const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
@@ -19,10 +21,44 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log("res in put request: ", res); 
+ 
+
+      updateColors(
+        colors.map((color) => {
+          if (color.id === colorToEdit.id) {
+          //   return res.data; //  single source of truth
+            return res.data;
+          } else {
+            return color;
+          }
+        })
+      );
+      
+      setColorToEdit(initialColor);// resets form state
+      setEditing(false); // reset state to not editing
+    })
+    .catch(err => console.log(err));
 
   };
 
-  const deleteColor = color => {
+  const deleteColor = colorToDelete => {
+
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${colorToDelete.id}`)
+    .then(res => {
+      console.log("response from deleteColor: ", res.data);
+
+      const updatedColors = colors.filter(color => {
+        return color.id !== colorToDelete.id
+      });
+      
+      updateColors(updatedColors);    
+
+    })
+    .catch(err => console.log("error:", err))
+
   };
 
   return (
